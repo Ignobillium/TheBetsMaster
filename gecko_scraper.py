e7daf88d421f315cdb13975b1fe4c4273eb20917
Ignobillium <Ignobillium@yandex.ru>
2020-02-05 17:04:28 +0300

    lock update

--- gecko_scraper.py
+++ gecko_scraper.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -23,7 +24,7 @@ class GeckoScraper:
     def headless():
         return  GeckoScraper(options = GeckoScraper.get_headless_options())
 
-    def __init__(self, options=None):
+    def __init__(self, options=None, lock=None):
         gecko_path = '/home/ignobillium/dev/geckodriver'
 
         if options is None:
@@ -41,6 +42,14 @@ class GeckoScraper:
         self.tabs = {}
         self.tabs[self.current_url] = self.current_tab
 
+        if lock is None:
+            self.lock = asyncio.Lock()
+        elif isinstance(lock, asyncio.Lock):
+            self.lock = lock
+        else:
+            logging.error("`lock` param must be asyncio.Lock obj, not %s" % type(lock))
+            raise TypeError("`lock` param must be asyncio.Lock obj, not %s" % type(lock))
+
     def __del__(self):
         if self.driver is not None:
             self.driver.quit()
@@ -54,16 +63,17 @@ class GeckoScraper:
             return
 
         # switch to last tab
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-        self.driver.execute_script('window.open("%s");' % url)
-        self.driver.switch_to.window(self.driver.window_handles[-1])
+        with await self.lock:
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+            self.driver.execute_script('window.open("%s");' % url)
+            self.driver.switch_to.window(self.driver.window_handles[-1])
 
-        self.tabs[url] = self.current_tab
-        self.current_url = url
-        self.current_tab = self.driver.current_window_handle
+            self.tabs[url] = self.current_tab
+            self.current_url = url
+            self.current_tab = self.driver.current_window_handle
 
-        # заменить на "дождаться критерия загрузки страницы"
-        await asyncio.sleep(10)
+            # заменить на "дождаться критерия загрузки страницы"
+            await asyncio.sleep(10)
 
 
     def switch_to_tab(self, url):
@@ -82,7 +92,8 @@ class GeckoScraper:
         if url not in self.tabs.keys():
             await self.new_tab(url)
 
-        if self.current_url != url:
+        with await self.lock:
             self.switch_to_tab(url)
+            source = self.driver.page_source
 
-        return self.driver.page_source
+        return source
